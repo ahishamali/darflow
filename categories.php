669ebddb28f6b8344e8df286727e4ced97991aad
AlexGuMusic <y-n7@live.com>
2016-03-24 19:01:59 +0300

    New_Cat
    fix the admin href.
    and fix the <th> tags

--- categories.php
+++ categories.php
@@ -1,5 +1,6 @@
 <?php
 //start
+
 require_once 'include/init.php';
 
 $login = new login();
@@ -36,37 +37,38 @@ if (isset($_REQUEST['search']) && !empty($_REQUEST['search'])) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>DarFlow - Categories </title>
-    <!-- Bootstrap core CSS -->
-       <link rel="stylesheet" href="lib/css/bootstrap.min.css">
-  <script src="lib/js/jquery.min.js"></script>
-  <script src="lib/js/bootstrap.min.js"></script>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>DarFlow - Categories </title>
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="lib/css/bootstrap.min.css">
+        <script src="lib/js/jquery.min.js"></script>
+        <script src="lib/js/bootstrap.min.js"></script>
         <link href="lib/css/bootstrap.css" rel="stylesheet">
         <link href="lib/css/font-awesome.css" rel="stylesheet">
         <!-- Custom styles for this template -->
         <link href="lib/css/style.css" rel="stylesheet">
-  </head>
+        <script src="lib/js/main.js"></script>
+    </head>
 
-  <body>
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="index.php">DarFlow</a>
-    </div>
+    <body>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php">DarFlow</a>
+                </div>
 
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
-    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" class="navbar-form navbar-left" role="search">
+                    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" class="navbar-form navbar-left" role="search">
                         <div class="form-group">
                             <input class="form-control" name="search" placeholder="Search" type="text">
                         </div>
@@ -76,39 +78,42 @@ if (isset($_REQUEST['search']) && !empty($_REQUEST['search'])) {
                             <input type="hidden" name="cat_id">
                             <input type="hidden" name="sort">
                         <?php } ?>
-                            <!--end-->
+                        <!--end-->
                         <button type="submit" class="btn btn-default" title="Search"><i class="glyphicon glyphicon-search"></i></button>
 
                     </form>
-        <ul class="nav navbar-nav">
-                        <li><a href="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);echo (isset($_REQUEST['advance']))?"":"?advance"; ?>" title="Advance Search"><i class="glyphicon glyphicon-cog"></i></a></li>
+                    <ul class="nav navbar-nav">
+                        <li><a href="<?php
+                            echo htmlspecialchars($_SERVER['PHP_SELF']);
+                            echo (isset($_REQUEST['advance'])) ? "" : "?advance";
+                            ?>" title="Advance Search"><i class="glyphicon glyphicon-cog"></i></a></li>
                     </ul>
-        
-      <ul class="nav navbar-nav navbar-right">
-          <li><a href="#">Admin Panel</a></li>
-          <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">UserName <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#change_pass" data-toggle="modal">Change Password</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Logout</a></li>
-            
-          </ul></li>
-      </ul>
-    </div>
-  </div>
-     <!--advance search tools-->
-     <!--advance search tools-->
+
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="cms/index.php">Admin Panel</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><?php echo $_SESSION['user_name']; ?><span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="#change_pass" data-toggle="modal">Change Password</a></li>
+                                <li class="divider"></li>
+                                <li><a href="?logout">Logout</a></li>
+
+                            </ul></li>
+                    </ul>
+                </div>
+            </div>
+            <!--advance search tools-->
+            <!--advance search tools-->
             <div id="adv_search" style="background: black;display: none;">
                 <div class="col-lg-2" style="margin-left: 96px;">
                     <select id="cat_id" class="form-control">
-                    <option disabled selected>-- Select Category --</option>
-                    <?php foreach ($cat->get_cat() as $row) { ?>
-                        <option value="<?php echo $row['cat_id']; ?>"><?php echo $row['cat_title']; ?></option>
-                    <?php } ?>
-                </select>
+                        <option disabled selected>-- Select Category --</option>
+                        <?php foreach ($cat->get_cat() as $row) { ?>
+                            <option value="<?php echo $row['cat_id']; ?>"><?php echo $row['cat_title']; ?></option>
+<?php } ?>
+                    </select>
                 </div>
-                 <div class="col-lg-2">
+                <div class="col-lg-2">
                     <select id="sort" class="form-control">
                         <option disabled selected>-- Select sort option --</option>
                         <option value="asc">Asc</option>
@@ -117,174 +122,123 @@ if (isset($_REQUEST['search']) && !empty($_REQUEST['search'])) {
                 </div>
             </div>
             <!--end-->
-</nav>
-<!--start-->
+        </nav>
+        <!--start-->
         <?php if (isset($_REQUEST['search']) && !empty($_REQUEST['search'])) { ?>
             <?php require_once 'search.php'; ?>
-        <?php } else { ?>
-<!--end-->
+<?php } else { ?>
+            <!--end-->
 
-    <section>
-        <div class="container">
-        <div class="row">
-        <div class="col-md-4">
-        <div class="list-group">
-          <a href="archive.php" class="list-group-item">
-            <i class="fa fa-file"></i> Archive
-          </a>
-          <a href="inbox.php" class="list-group-item"><i class="fa fa-download"></i> Inbox</a>
-          <a href="outbox.php" class="list-group-item"><i class="fa fa-upload"></i> Outbox</a>
-          <a href="categories.php" class="list-group-item active"><i class="fa fa-th"></i> Categories</a>
+            <section>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="list-group">
+                                <a href="archive.php" class="list-group-item">
+                                    <i class="fa fa-file"></i> Archive
+                                </a>
+                                <a href="inbox.php" class="list-group-item"><i class="fa fa-download"></i> Inbox</a>
+                                <a href="outbox.php" class="list-group-item"><i class="fa fa-upload"></i> Outbox</a>
+                                <a href="categories.php" class="list-group-item active"><i class="fa fa-th"></i> Categories</a>
 
-        </div>
-        </div>
-        <div class="col-md-8">
-        <h1 class="page-header"> <i class="fa fa-th"></i> Categories </h1>
-        <h3>Ministry</h3>
-        <table class="table table-striped">
-          <tr>
-          <th>Doc ID</th>
-          <th>Doc Title</th>
-          <th>Date</th>
-          <th>Sender</th>
-          <th>Receiver</th>
-          <th>Sent</th>
-          <th>Received</th>
-          <th>Replayed</th>
-          <th>Replay Received</th>
-          </tr>
-          <tr>
-          <td>Doc101</td>
-          <td>Doc one</td>
-          <td>12/12/12</td>
-          <td>Sender one</td>
-          <td>Receiver one</td>
-          <td>check</td>
-          <td>check</td>
-          <td>check</td>
-          <td>check</td>
-          </tr>
-           <tr>
-          <td>Doc101</td>
-          <td>Doc one</td>
-          <td>12/12/12</td>
-          <td>Sender one</td>
-          <td>Receiver one</td>
-          <td>check</td>
-          <td>check</td>
-          <td>check</td>
-          <td>check</td>
-          </tr>
-          <tr>
-          <td>Doc101</td>
-          <td>Doc one</td>
-          <td>12/12/12</td>
-          <td>Sender one</td>
-          <td>Receiver one</td>
-          <td>check</td>
-          <td>check</td>
-          <td>check</td>
-          <td>check</td>
-          </tr>
-        </table>
-        <br>
-        <hr>
-        <h3>Administration</h3>
-        <table class="table table-striped">
-            <tr>
-          <th>Doc ID</th>
-          <th>Doc Title</th>
-          <th>Date</th>
-          <th>Sender</th>
-          <th>Receiver</th>
-          <th>Sent</th>
-          <th>Received</th>
-          <th>Replayed</th>
-          <th>Replay Received</th>
-          </tr>
-          <tr>
-          <td>Doc101</td>
-          <td>Doc one</td>
-          <td>12/12/12</td>
-          <td>Sender one</td>
-          <td>Receiver one</td>
-          <td>check</td>
-          <td>check</td>
-          <td>check</td>
-          <td>check</td>
-          </tr>
-           <tr>
-          <td>Doc101</td>
-          <td>Doc one</td>
-          <td>12/12/12</td>
-          <td>Sender one</td>
-          <td>Receiver one</td>
-          <td>check</td>
-          <td>check</td>
-          <td>check</td>
-          <td>check</td>
-          </tr>
-          <tr>
-          <td>Doc101</td>
-          <td>Doc one</td>
-          <td>12/12/12</td>
-          <td>Sender one</td>
-          <td>Receiver one</td>
-          <td>check</td>
-          <td>check</td>
-          <td>check</td>
-          <td>check</td>
-          </tr>
-        </table>
-        </div>
-        </div>
-    </section>
-        <?php }?>
- <!--start-->
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                            <h1 class="page-header"> <i class="fa fa-th"></i> Categories </h1>
+                            <!--start-->
+                            <?php foreach ($cat->get_cat() as $row) { ?>
+                                <h3><?php echo $row['cat_title'];?></h3>
+                            <!--end-->
+                            <table class="table table-striped">
+                                <tr>
+                                    <th>Doc ID</th>
+                                    <th>Doc Title</th>
+                                    <th>Date</th>
+                                    <th>Sender</th>
+                                    <th>Receiver</th>
+                                    <th>Created by</th>
+                                    <th>Category</th>
+                                    <th>Issue Date</th>
+                                </tr>
+                               <?php foreach ($inbox->get_by_cat_id($row['cat_id']) as $rows){ ?>
+                                <tr>
+                                    <td><?php echo $rows['doc_id']; ?></td>
+                                    <td><?php echo $rows['doc_title']; ?></td>
+                                    <td>12/12/12</td>
+                                    <td>Sender one</td>
+                                    <td>Receiver one</td>
+                                    <td>check</td>
+                                    <td>check</td>
+                                    <td>check</td>
+                                    
+                                </tr>
+                            <?php } ?>
+                                <?php foreach ($outbox->get_by_cat_id($row['cat_id']) as $rows){ ?>
+                                <tr>
+                                    <td><?php echo $rows['doc_id']; ?></td>
+                                    <td><?php echo $rows['doc_title']; ?></td>
+                                    <td>12/12/12</td>
+                                    <td>Sender one</td>
+                                    <td>Receiver one</td>
+                                    <td>check</td>
+                                    <td>check</td>
+                                    <td>check</td>
+                                    
+                                </tr>
+                            <?php } ?>
+                            </table>
+                            <?php } ?>
+                            <br>
+                            <hr>
+                        </div>
+                    </div>
+            </section>
+<?php } ?>
+        <!--start-->
         <div class="modal" id="change_pass" >
             <div class="modal-dialog modal-sm">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h4>Change password</h4>
-            </div>
-                   <div class="modal-body modal-sm">
-                    <div class="form-group">
-                        <form action="" method="post">
-                            <input type="password" class="form-control" name="prev_pass" placeholder="Enter cuerrent Password"><br>
-                            <input type="password" class="form-control"  name="new_pass" placeholder="Enter new Password"><br>
-                            <input type="password" class="form-control"  name="con_pass" placeholder="confirm new Password"><br>
-                            <input type="submit">
-                        </form>
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4>Change password</h4>
                     </div>
+                    <div class="modal-body modal-sm">
+                        <div class="form-group">
+                            <form action="" method="post">
+                                <input type="password" class="form-control" required="" name="prev_pass" placeholder="Enter cuerrent Password"><br>
+                                <input type="password" class="form-control" required="" name="new_pass" placeholder="Enter new Password"><br>
+                                <input type="password" class="form-control" required="" name="con_pass" placeholder="confirm new Password"><br>
+                                <input type="submit">
+                            </form>
+                        </div>
                     </div>
-            </div>
+                </div>
             </div>
         </div>
         <!--end-->
-<footer>
-<p>Copyright 2015, All Rights Reserved</p>
-</footer>
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-        
+        <footer>
+            <p>Copyright 2015, All Rights Reserved DarFlow</p>
+        </footer>
+        <!-- Bootstrap core JavaScript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+
         <script src="lib/js/jquery.tablesorter.js"></script>
-    <script>
-	    $(function(){
-      $('#sort-table').tablesorter({
-        sortList:[[0,0], [1,0]]
-      });
-    });
-	</script>
-       <script>
-      var $rows = $('table tr');
-      $('.search-form').keyup(function() {
-        var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
-        $rows.show().filter(function() {
-          var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
-          return !~text.indexOf(val);
-        }).hide();
-      });
+        <script>
+            $(function () {
+                $('#sort-table').tablesorter({
+                    sortList: [[0, 0], [1, 0]]
+                });
+            });
+        </script>
+        <script>
+            var $rows = $('table tr');
+            $('.search-form').keyup(function () {
+                var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+                $rows.show().filter(function () {
+                    var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+                    return !~text.indexOf(val);
+                }).hide();
+            });
             //            start
 <?php if (isset($_REQUEST['advance'])) { ?>
                 $('#adv_search').show();
@@ -298,10 +252,7 @@ if (isset($_REQUEST['search']) && !empty($_REQUEST['search'])) {
             $('#sort').change(function () {
                 $('input[name=sort]').val($('#sort').val());
             });
-            $('tr').click(function () {
-                alert('jack ass');
-            });
-//                end
-    </script>
-  </body>
+            //                end
+        </script>
+    </body>
 </html>
